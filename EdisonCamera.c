3331d7fd34573caea3be53fc1b3b2bbfc86748cc
Mickael MAYEUR <mickael@mayeur.eu>
2015-05-11 14:44:22 +0200

    Remove c# and go to c with opencv

---
+++ EdisonCamera.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <opencv/highgui.h>
+#include <opencv/cv.h>
+ 
+int main() {
+ 
+	int run = 1;
+    // Image
+     IplImage *image;
+    // Capture vidéo
+    CvCapture *capture;
+ 
+    capture = cvCreateCameraCapture(CV_CAP_ANY);
+ 
+    // Vérifier si l'ouverture du flux est ok
+    if (!capture) {
+ 
+       printf("Can't open video !\n");
+       return -1;
+ 
+    }
+	else
+	{
+		printf("Everything is OK!\n");
+
+	}
+ 
+ 
+    while(run) {
+ 
+       // getting image
+       image = cvQueryFrame(capture);
+ 
+ 
+       // waiting 10ms
+	   usleep(10);
+ 
+    }
+ 
+    cvReleaseCapture(&capture);
+ 
+    return 0;
+ 
+}
