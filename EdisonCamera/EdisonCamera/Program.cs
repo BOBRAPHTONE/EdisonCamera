9a3df462b715450e6ee33320d4e5e4bb5347dbb7
Mickael MAYEUR <mickael@mayeur.eu>
2015-05-11 13:28:03 +0200

    use opencvsharp

--- EdisonCamera/EdisonCamera/Program.cs
+++ EdisonCamera/EdisonCamera/Program.cs
@@ -1,96 +1,51 @@
 ﻿using System;
-using AForge.Video;
-using AForge.Video.DirectShow;
 using System.Drawing;
+using OpenCvSharp.CPlusPlus;
 
 namespace EdisonCamera
 {
     class Program
     {
-       
-        static void Main(string[] args)
-        {
-            new Camera();
 
+        private static void Main(string[] args)
+        {
+
+            VideoCapture cap = new VideoCapture(0);
+            if (!cap.IsOpened()) // if not success, exit program
+            {
+                Console.WriteLine("Cannot open the web cam");
+                return;
+            }
+
+            while (true)
+            {
+                //     try
+                {
+                    Mat imgOriginal = new Mat();
+                    cap.Read(imgOriginal); // read a new frame from video
+                    //      Cv2.ImShow("Original", imgOriginal); //show the original image
+
+                    if (Cv2.WaitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+                    {
+                        Console.WriteLine("esc key is pressed by user");
+                        break;
+                    }
+                }
+                //         catch (Exception)
+                {
+
+                }
+            }
         }
     }
     
     class Camera
     {
-        private bool DeviceExist = false;
-        private FilterInfoCollection videoDevices;
-        private VideoCaptureDevice videoSource = null;
-
-
-        private void showCamList()
-        {
-            try
-            {
-                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
-                if (videoDevices.Count == 0)
-                    throw new ApplicationException();
-
-                DeviceExist = true;
-                if (videoDevices.Count == 1) { 
-                    Console.WriteLine(videoDevices[0].Name);
-                  }
-                else
-                foreach (FilterInfo device in videoDevices)
-                {
-                    Console.WriteLine(device.Name);
-                }
-            }
-            catch (ApplicationException)
-            {
-                DeviceExist = false;
-                Console.WriteLine("No capture device on your system");
-                Console.ReadLine();
-                System.Environment.Exit(-1);
-            }
-        }
+        
+       
         public Camera()
         {
-            this.showCamList();
-            if (DeviceExist)
-            {
-                videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
-                videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
-                CloseVideoSource();
-/*
- //Show all the output possibilities
-                for (int i = 0; i < videoSource.VideoCapabilities.Length; i++)
-                {
-
-
-                    Console.WriteLine("Resolution Number " + i);
-                    Console.WriteLine(videoSource.VideoCapabilities[i].FrameSize);
-                }
- */
-  
-                videoSource.VideoResolution = videoSource.VideoCapabilities[1];
-                
-                //videoSource.DesiredFrameSize = new Size(160, 120);
-                //videoSource.DesiredFrameRate = 10;
-                videoSource.Start();
-                Console.WriteLine("Everything is Okay");
-                videoSource.Stop();
-               // videoSource.Stop();
-            }
-        }
-        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
-        {
-            //Bitmap img = (Bitmap)eventArgs.Frame.Clone();
-            //do processing here
-            //send to LCD
-        }
-        private void CloseVideoSource()
-        {
-            if (!(videoSource == null))
-                if (videoSource.IsRunning)
-                {
-                    videoSource.SignalToStop();
-                    videoSource = null;
-                }
+           
         }
 
     }
